fb05780856c4c803c1fcc4613f75404d87ef4ef5
Alexandre MOTTIER <amottier@e1r12p7.42.fr>
2017-07-25 21:14:02 +0200

    check

---
+++ check_is_valid.c
@@ -0,0 +1,55 @@
+int check_is_valid(char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[i] != '\0')
+  {
+    if (str[i] != '.' || str[i] != 'o')
+        return (0);
+    i++;
+  }
+  return (1)
+}
+
+int   check_is_valid_number(char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[k] != '\n')
+      k++;
+  while (str[i] '\0')
+  {
+    j = 0;
+    while (str[i] != '\n')
+    {
+      j++;
+    }
+    if (j != k)
+      return (0)
+    i++;
+  }
+  return (1);
+}
+
+int count line(char *str)
+{
+  int i;
+  int j;
+
+  i = 0;
+  j = 0;
+  while (str[i] != '\0')
+  {
+    if (str[i] == '\n')
+        j++;
+    i++;
+  }
+  return (j);
+}
+
+char **creat_double_tab(char *str)
+{
+
+}
